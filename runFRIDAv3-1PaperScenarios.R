e0b93672886bbf95f99ffcc5fde610801ffeeab1
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2026-08-18 16:34:56 +0200

    setup

---
+++ runFRIDAv3-1PaperScenarios.R
@@ -0,0 +1,13 @@
+
+csv_files <- list.files("ScenarioFiles", pattern = "\\.csv$", full.names = TRUE)
+
+for (csv_file in csv_files) {
+  
+  file_path <- csv_file
+  file_name <- basename(csv_file)
+  
+  # --- Your code below ---
+  
+  # --- End of your code ---
+  
+}
